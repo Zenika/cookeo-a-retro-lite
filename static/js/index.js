f130469a1e2f5d056bd80c6dcbebf87daa656195
Julie Planque <28594096+julie-planque@users.noreply.github.com>
2024-07-18 17:32:40 +0200

    [FRONT] Indentation code

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, {});
+
     // Get the buttons
     const advancedButton = document.getElementById('advancedButton');
 
     // Add event listerners to the buttons
-    advancedButton.addEventListener('click', function() {
+    advancedButton.addEventListener('click', function () {
         const customOptions = document.getElementById('advancedOptions');
         if (customOptions.style.display === 'none') {
             customOptions.style.display = 'block';
         } else {
             customOptions.style.display = 'none';
-        }   
+        }
     });
 });
 
